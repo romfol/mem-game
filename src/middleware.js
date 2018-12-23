eafb984c508258a58b4a16fbecc34d839d28f58a
romfol <romanfoltyn1@gmail.com>
2018-12-23 16:25:17 +0200

    matching

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,9 @@
 export const middleware = store => next => action => {
   if (action.type === 'SELECT') {
-    console.log(action.payload);
+    let selected = store.getState().selected;
+    if (selected === action.payload) {
+      alert("it's a match");
+    }
   }
   return next(action);
 };
